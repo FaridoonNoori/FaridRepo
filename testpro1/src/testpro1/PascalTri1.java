b941f6be3a193a3e9a75f298a2a211b52e2bb82b
f2nou <f2nou@LAPTOP-DV3EQILT.myfiosgateway.com>
2021-05-07 18:12:51 -0400

    Writing code

--- testpro1/src/testpro1/PascalTri1.java
+++ testpro1/src/testpro1/PascalTri1.java
@@ -1,5 +1,26 @@
 package testpro1;
 
+import java.util.ArrayList;
+
 public class PascalTri1 {
 
+	public void pascal( int n) {
+		
+		int[][] ar= new int[n][n];
+		
+		for(int j=0; j<n; ++j)
+		{
+			
+			for(int i=0; i<=j; ++i) 
+			{
+				if(i==j || i==0)  ar[j][i]=1;	
+				else
+						ar[j][i]=ar[j-1][i]+ar[j-1][i-1];
+						System.out.print("       "+ar[j][i]);
+				
+			}
+			System.out.println("");
+		}
+		
+	}
 }
--- testpro1/src/testpro1/Test.java
+++ testpro1/src/testpro1/Test.java
@@ -5,7 +5,12 @@ class Test {
 	 int x = -5;
 	 System.out.println(x<<2); 
 	 int y = 4;
-	 System.out.println(y>>1); 
+	 System.out.println( y>>1); 
+	 
+	 System.out.println();
+	 PascalTri1 pas= new PascalTri1();
+	 pas.pascal(8);
 	 } 
-	}
+	 
+}
 
